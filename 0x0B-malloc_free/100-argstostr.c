2839b35fa4382cf95822ed2a0dfe5de24099a74b
Queengeorge <queenkingsleygeorge@gmail.com>
2023-03-14 16:59:49 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,7 +4,6 @@
  * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
- *
  * Return: 0
  */
 char *argstostr(int ac, char **av)
@@ -13,19 +12,22 @@ char *argstostr(int ac, char **av)
 	char *str;
 
 	if (ac == 0 || av == NULL)
-
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
 			l++;
-		l += ac;
-		str = malloc(sizeof(char) * l + 1);
-		if (str == NULL)
-			return (NULL);
-		for (i = 0; i < ac; i++)
-		for (n = 0; av[i][n]; n++)
-		{
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
 		str[r] = av[i][n];
 		r++;
 	}
@@ -33,6 +35,6 @@ char *argstostr(int ac, char **av)
 	{
 		str[r++] = '\n';
 	}
-}
-return (str);
+	}
+	return (str);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- *  count_word - helper function to count the number of words in a string
- *  @s: string to evaluate
- *  Return: number of words
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
  */
 int count_word(char *s)
 {
@@ -11,25 +13,28 @@ int count_word(char *s)
 
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
 			flag = 0;
-		else
-			if (flag == 0)
-			{
-				flag = 1;
-				w++;
-			}
-		return (w);
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
 	}
+
+	return (w);
+}
 /**
- * strtow - splits a string into words
+ * **strtow - splits a string into words
  * @str: string to split
+ *
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-	char **strtow(char *str)
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
@@ -39,9 +44,11 @@ int count_word(char *s)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
+
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -63,6 +70,8 @@ int count_word(char *s)
 		else if (c++ == 0)
 			start = i;
 	}
+
 	matrix[k] = NULL;
+
 	return (matrix);
 }
