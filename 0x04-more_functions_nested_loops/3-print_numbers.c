94ac122f215daac4eebb922b1b4fd32c74f8bb6c
Queengeorge <queenkingsleygeorge@gmail.com>
2023-04-03 08:34:03 -0700

    updating files

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
+ * print_numbers - print numbers that are from  0-9
  * return: void
  */
 
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * more_numbers - print numbers ten times
  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 14; j++)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
+			if (j > 9)
+			{
+			_putchar(j / 10 + '0');
 		}
+			{
+			_putchar (j % 10 + '0');
+			}
 		_putchar('\n');
 	}
 }
